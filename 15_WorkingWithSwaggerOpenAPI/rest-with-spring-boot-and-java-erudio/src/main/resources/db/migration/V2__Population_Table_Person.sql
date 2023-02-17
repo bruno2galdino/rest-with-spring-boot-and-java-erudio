abc7322e5ac175666d5ad8534aad6d29826bcc7d
Galdino <bruno2galdino@gmail.com>
2023-02-17 17:34:08 -0300

    Swagger

---
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Population_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Tatui-SP', 'Bruno', 'Male', 'Galdino'),
+	(3, 'Tatui-SP', 'Ana Luiza', 'Female', 'Oliveira Galdino'),
+	(4, 'Tatui-SP', 'Maria Clara', 'Female', 'Oliveira Galdino'),
+	(6, 'Primavera do Lest - MT', 'Manoel', 'Male', 'Carvalho - Gerente'),
+	(7, 'Tatui - SP', 'Paulo', 'Male', 'Favoretti'),
+	(8, 'Tatui - SP', 'Ronei', 'Male', 'Antunes');
